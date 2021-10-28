a6b529021739a379e93ff69f113416acc4b841d3
Sherif Kamal <sherif.kamal@vodafone.com>
2021-10-28 22:25:36 +0200

    Initial Commit

---
+++ EventsCore/EventsCore.h
@@ -0,0 +1,18 @@
+//
+//  EventsCore.h
+//  EventsCore
+//
+//  Created by Sherif Kamal on 10/28/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EventsCore.
+FOUNDATION_EXPORT double EventsCoreVersionNumber;
+
+//! Project version string for EventsCore.
+FOUNDATION_EXPORT const unsigned char EventsCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventsCore/PublicHeader.h>
+
+
