ba160578f909eb7209635dff3b95f541470d284f
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-10-30 13:59:01 +0200

    Add `EventsPlatform` framework for iOS platform-specific components

---
+++ EventsPlatform/EventsPlatform.h
@@ -0,0 +1,18 @@
+//
+//  EventsPlatform.h
+//  EventsPlatform
+//
+//  Created by Sherif Kamal on 10/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EventsPlatform.
+FOUNDATION_EXPORT double EventsPlatformVersionNumber;
+
+//! Project version string for EventsPlatform.
+FOUNDATION_EXPORT const unsigned char EventsPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventsPlatform/PublicHeader.h>
+
+
