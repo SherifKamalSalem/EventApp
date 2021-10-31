417be5cbfca729c7086f00331d47d3ac377f4800
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-10-31 20:43:18 +0200

    Create storyboard for main screen with event types and connect MainViewController to it

---
+++ EventsPlatform/Event Types UI/Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  EventsPlatform
+//
+//  Created by Sherif Kamal on 10/31/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
