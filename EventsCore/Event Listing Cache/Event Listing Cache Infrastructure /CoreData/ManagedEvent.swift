413cc8d1cee54892f3a416933ea8251f52bd1c14
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-11-03 00:01:27 +0200

    Create CoreDate event NSManagedObject

---
+++ EventsCore/Event Listing Cache/Event Listing Cache Infrastructure /CoreData/ManagedEvent.swift
@@ -0,0 +1,41 @@
+//
+//  ManagedEvent.swift
+//  EventsCore
+//
+//  Created by Sherif Kamal on 11/2/21.
+//
+//
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedEvent: NSManagedObject {
+    @NSManaged internal var id: String
+    @NSManaged internal var eventDescription: String?
+    @NSManaged internal var name: String
+    @NSManaged internal var longitude: String
+    @NSManaged internal var latitude: String
+    @NSManaged internal var startDate: String
+    @NSManaged internal var cover: String
+}
+
+extension ManagedEvent {
+    
+    internal static func events(from localEvents: [LocalEventDTO], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localEvents.map { local in
+            let managed = ManagedEvent(context: context)
+            managed.id = local.id
+            managed.eventDescription = local.eventDescription
+            managed.name = local.name
+            managed.cover = local.cover
+            managed.longitude = local.longitude
+            managed.latitude = local.latitude
+            managed.startDate = local.startDate
+            return managed
+        })
+    }
+    
+    internal var local: LocalEventDTO {
+        return LocalEventDTO(id: id, name: name, longitude: longitude, latitude: latitude, startDate: startDate, eventDescription: eventDescription, cover: cover)
+    }
+}
+
--- EventsCore/Event Listing Cache/LocalEventDTO.swift
+++ EventsCore/Event Listing Cache/LocalEventDTO.swift
@@ -10,22 +10,21 @@ import Foundation
 public struct LocalEventDTO: Equatable, Hashable {
     let id: String
     let name: String
-    let longitude, latitude, endDate, startDate: String
-    let description: String
+    let longitude, latitude, startDate: String
+    let eventDescription: String?
     let cover: String
     
     public init(id: String, name: String,
                 longitude: String,latitude: String,
-                startDate: String, endDate: String,
-                description: String, cover: String
+                startDate: String, eventDescription: String?,
+                cover: String
     ) {
         self.id = id
         self.name = name
         self.longitude = longitude
         self.latitude = latitude
         self.startDate = startDate
-        self.endDate = endDate
-        self.description = description
+        self.eventDescription = eventDescription
         self.cover = cover
     }
 }
