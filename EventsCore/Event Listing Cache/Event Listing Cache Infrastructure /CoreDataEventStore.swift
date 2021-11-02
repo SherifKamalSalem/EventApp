9a6ebdbca0175fb1db773a4a02f3f0f5d5c7ea00
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-11-03 00:03:07 +0200

    Create EventStore implementation and implement insert function

---
+++ EventsCore/Event Listing Cache/Event Listing Cache Infrastructure /CoreDataEventStore.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataEventStore.swift
+//  EventsCore
+//
+//  Created by Sherif Kamal on 11/2/21.
+//
+
+import CoreData
+
+public final class CoreDataEventStore: EventStore {
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "EventStore", url: storeURL, in: bundle)
+        context = container.newBackgroundContext()
+    }
+    
+    public func insert(_ localEvents: [LocalEventDTO], completion: @escaping InsertionCompletion) {
+        perform { context in
+            do {
+                _ = ManagedEvent.events(from: localEvents, in: context)
+                try context.save()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
+    }
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
+}
